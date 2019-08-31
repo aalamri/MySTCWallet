cfe65005b1b6afcbb029e6b7bebb2706c36d7665
Ali <ali@freshbasketsa.com>
2019-08-31 17:14:53 +0300

    first commit

---
+++ src/main/java/com/demo/MySTCWallet/MyStcWalletApplication.java
@@ -0,0 +1,13 @@
+package com.demo.MySTCWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStcWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStcWalletApplication.class, args);
+	}
+
+}
